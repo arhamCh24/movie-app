d7f7ba261947e63ecdefb42d2a96c4fe719bc2e1
arhamCh24 <ch.arham2001@gmail.com>
2025-05-12 15:38:04 -0400

    added API to fetch movies from 'tmdb', added new 'Spinner' and 'MovieCard' Components, and used api to display movie details in MovieCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
+
+// API - Apllication Programming Interface - a set of rules that allows one software application to talk to another
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, SetErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    SetErrorMessage("");
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      // throw new Error("Failed to fetch movies"); // this is just for test purpose
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+
+      if (data.response === "false") {
+        SetErrorMessage(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      SetErrorMessage("Error fetching movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
@@ -14,10 +69,26 @@ const App = () => {
             Find <span className="text-gradient">Movies</span>You'll Love
             Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className="text-white"> {searchTerm}</h1>
+        {/* <h1 className="text-white"> {searchTerm}</h1> */}
+
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
+
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
   );
